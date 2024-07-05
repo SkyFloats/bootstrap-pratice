abca5e578b4edeca3d4d107d019991d439bfa697
SkyFloats <dan17172017@gmail.com>
2024-07-05 15:54:48 -0300

    first commit

---
+++ constants.js
@@ -0,0 +1,7 @@
+const URL_API_BASIC =
+  "https://my-json-server.typicode.com/profgarrido/basics-recipes";
+
+const URL_API_ADITIONAL =
+  "https://my-json-server.typicode.com/profgarrido/indication-recipes";
+
+export { URL_API_BASIC, URL_API_ADITIONAL };
